9b076641485171d1a8d1010dc8fd6acc2e8522c9
Formu-Git <79724463+Formu-Git@users.noreply.github.com>
2021-03-15 16:37:55 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from os import *
+
+path_1 = "path" #path of file folder's
+path_2 = "path" #path of folder where you want organize floders
+extensions = ['.txt', '.png', '.jpg'] #extensions
+
+
+for file in listdir(path_1):
+    for ext in extensions:
+        if file.endswith(ext):
+            if path.exists(path_2 + "\{}".format(ext + "Files")):
+                print("...")
+                ext_folders = path_2 + "\{}".format(ext + "Files")
+                rename(path_1 + "\{}".format(file), ext_folders + "\{}".format(file))
+                print("success !")
+            else:
+                mkdir(path_2 + "\{}".format(ext + "Files"))
+                print(path_2 + "\{}".format(ext + "Files"))
+                ext_folders = path_2 + "\{}".format(ext + "Files")
+                rename(path_1 + "\{}".format(file), ext_folders + "\{}".format(file))
+                print("success !")
