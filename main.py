ce4b7e098cbed095483162ceac73f071b34b758b
Formu-Git <79724463+Formu-Git@users.noreply.github.com>
2021-03-17 00:15:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ for file in listdir(path_1):
             else:
                 print("...")
                 mkdir(path_2 + "\{}".format(ext + "Files"))
-                print(path_2 + "\{}".format(ext + "Files"))
                 ext_folders = path_2 + "\{}".format(ext + "Files")
                 rename(path_1 + "\{}".format(file), ext_folders + "\{}".format(file))
                 print("success !")
